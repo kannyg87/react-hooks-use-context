2dc0be93ab89fa9738e922e67601b5aed923239e
Kanny <kanny.ghafour87@gmail.com>
2024-04-30 15:40:53 -0500

    lab completed for user context

---
+++ src/context/user.js
@@ -0,0 +1,17 @@
+// src/context/user.js
+import React, { useState } from 'react';
+import user from '../data'
+
+
+const UserContext = React.createContext();
+// create a provider component
+function UserProvider({ children }) {
+  const [user, setUser] = useState(null);
+  return (
+    <UserContext.Provider value={{ user, setUser  }}>
+      {children}
+    </UserContext.Provider>
+  );
+}
+
+export { UserContext, UserProvider };
